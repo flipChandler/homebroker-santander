7e823e23f7f6f812e0af63c2033a100fd73ef954
flipChandler <felipe.santos11@hotmail.com>
2021-06-14 21:11:34 -0300

    StockDTO & StockController

---
+++ backend/src/main/java/br/com/santander/backend/controller/StockController.java
@@ -0,0 +1,20 @@
+package br.com.santander.backend.controller;
+
+import br.com.santander.backend.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save (@RequestBody StockDTO dto) {
+        return ResponseEntity.ok(dto);
+    }
+
+}
---
+++ backend/src/test/java/br/com/santander/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
